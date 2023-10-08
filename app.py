3c0c2631749a6f47e01ab3262aa45d7ca49c0b3e
Marketa Konecna <konecnamarketa98@seznam.cz>
2023-10-08 20:37:51 +0200

    temperature done

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
+import requests
+
 
 # Configure application
 app = Flask(__name__)
@@ -13,5 +15,22 @@ db = SQL("sqlite:///locations.db")
 
 @app.route("/", methods=["GET"])
 def index():
-    weather = db.execute("select * from locations")
-    return render_template("index.html" ,weather_s =weather)
+    pocasi = db.execute("select * from locations")
+    for slovnik in pocasi:
+        latitude = slovnik['latitude']
+        longitude = slovnik ['longitude']
+        teplota = weather (latitude,longitude)
+        slovnik ['teplota'] = teplota
+
+    return render_template("index.html" ,weather_s =pocasi)
+
+
+def weather(latitude,longitude):
+    PARAMS = {'latitude':latitude, 'longitude':longitude, 'hourly':'temperature_2m', 'current_weather':True}
+    response = requests.get (url ='https://api.open-meteo.com/v1/forecast', params = PARAMS)
+    data = response.json()
+    # print (data)
+    current_weather = data['current_weather']
+    temperature = current_weather['temperature']
+    return temperature
+
