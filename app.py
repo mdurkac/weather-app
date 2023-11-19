edac4fd9f373a17049a63940b98aa614a2fab2c7
Marketa Konecna <konecnamarketa98@seznam.cz>
2023-11-19 14:44:06 +0100

    done

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 import requests
-
+import datetime
 
 # Configure application
 app = Flask(__name__)
@@ -69,3 +69,38 @@ def delete():
         db.execute("delete from locations where id=?", id)
     return redirect("/")
 
+
+@app.route("/more_info", methods=['GET'])
+def more_info():
+    id = request.args.get('id')
+    list = db.execute("select * from locations where id=?", id)
+    slovnik = list [0]
+    latitude = slovnik['latitude']
+    longitude = slovnik ['longitude']
+    current, daily = get_forecast (latitude, longitude)
+    return render_template ('forecast_page.html', location= slovnik, weather=current, days=daily)
+
+def get_forecast(latitude, longitude):
+    PARAMS = {'latitude':latitude, 'longitude':longitude,'current':'temperature_2m,apparent_temperature,is_day,rain,showers,windspeed_10m','daily':'temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum,showers_sum,windspeed_10m_max', 'timezone':'auto' }
+    response = requests.get (url ='https://api.open-meteo.com/v1/forecast', params = PARAMS) 
+    data = response.json()
+    current = data ['current']
+    daily = data ['daily']
+    return current, seven_days(daily)
+
+
+def seven_days(daily):
+    new_list = []
+    time = daily['time']
+    temperature_2m_max = daily ['temperature_2m_max']
+    temperature_2m_min = daily ['temperature_2m_min']
+    for i in range(0,7,1):
+        day = func(time[i])
+        slovnik = {'time': day, 'temperature_2m_max': temperature_2m_max[i],'temperature_2m_min': temperature_2m_min[i] }
+        new_list.append(slovnik)
+    return new_list
+
+def func(date):
+    the_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    day = the_date.strftime('%a')
+    return day
